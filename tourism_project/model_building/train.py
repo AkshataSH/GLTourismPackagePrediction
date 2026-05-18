4bfb0b02bf5c9b509b10a8e171d9e61630c21e35
AkshataSh <akshata.sh11@gmail.com>
2026-05-18 13:16:20 +0530

    MlFlow updating public url

--- tourism_project/model_building/train.py
+++ tourism_project/model_building/train.py
@@ -26,7 +26,7 @@ from huggingface_hub.utils import RepositoryNotFoundError
 # MLflow
 import mlflow
 # Set up MLflow tracking
-mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_tracking_uri("https://undefined-illusive-dole.ngrok-free.dev")
 mlflow.set_experiment("ash-tourism-package-prediction")
 repo_id = "akshatash/TourismPkgPrediction"
 api = HfApi()
