ea0f3d99d33573b296f8d8afb80df7b8802b4868
AkshataSh <akshata.sh11@gmail.com>
2026-05-18 13:33:33 +0530

    OneHotEncoder missing

--- tourism_project/model_building/train.py
+++ tourism_project/model_building/train.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # Preprocessing & pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.compose import make_column_transformer
 from sklearn.pipeline import make_pipeline
 
